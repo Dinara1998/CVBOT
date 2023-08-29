176cd33ac74e4a56ce7c7add27bb4e061ea2c565
DINA <DINARA.YADGAROVA.98@BK.RU>
2023-08-29 18:20:53 +0300

    pre_realize version

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+import sqlite3
 import telebot
+
+
+
 bot = telebot.TeleBot("6338767781:AAEOMAifeZkYLzEg4yIodRckG2_JkMSfYjQ")
 
 ADMIN_USERNAME = "dinarayad"
+
+db = sqlite3.connect("db.db", check_same_thread=False)
+cursor = db.cursor()
+
